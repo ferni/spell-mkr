a440da4ccc5ff9a1f45d51873568e1da901a2849
Fernando Tiberti <fertiberti@gmail.com>
2018-04-08 22:33:33 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+const argv = process.argv;
+const parameter = argv[2];
+const value = argv[3];
+const mkrParams = ['cap', 'mat', 'tax', 'fee', 'axe', 'tubGap', 'tapGap', 'way', 'how'];
+
+if (argv.length === 2 || argv[2] === '--help') {
+  console.log(
+    'Usage: spell <maker parameter> <value> [--publish]\n' +
+    'Example: spell cap 50000000000000000000000000\n' +
+    '\nMaker Parameters:\n' +
+    '\t  cap : Debt ceiling\n' +
+    '\t  mat : Liquidation ratio\n' +
+    '\t  tax : Stability fee\n' +
+    '\t  fee : Governance fee\n' +
+    '\t  axe : Liquidation fee\n' +
+    '\t  tubGap : Join/Exit Spread\n' +
+    '\t  tapGap : Boom/Bust Spread\n' +
+    '\t  way : Rate of change of target price (per second)\n'
+  );
+  process.exit();
+}
+
+if (!mkrParams.includes(parameter)) {
+  console.log('Unrecognized parameter "' + parameter + "'. Call spell --help for a list of valid parameters.");
+  process.exit();
+}
+
+if (value === undefined) {
+  console.log('No value specified for ' + parameter);
+  process.exit();
+}
+
+const { execSync } = require('child_process');
+const method = 'set' + parameter.charAt(0).toUpperCase() + parameter.slice(1);
+const valueUint = execSync('seth --to-uint256 ' + value);
+const calldata = execSync(`seth calldata '${method}(uint256)' ${valueUint}`);
+console.log(calldata.toString());
